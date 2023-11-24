50a5ff77b93e1ae8ec42b5f181af1b5f9041e948
jonathanbensadoun <jonathan.ben-sadoun@oclock.school>
2023-11-24 18:57:25 +0100

    ajout animation sur la fusée  du pandora et du menu

--- public/js/index.js
+++ public/js/index.js
@@ -1,1 +1,16 @@
-console.log('hello');
+const menuBurger = {
+    handleClickMenuBurger: function() {
+            const menu = document.querySelector('.container-burger-menu');
+            menu.classList.toggle('hidden');
+    },
+    init: function(){
+        const btnMenuBurger = document.querySelector('.btn-menu');
+        btnMenuBurger.addEventListener('click',menuBurger.handleClickMenuBurger);
+
+
+
+    }
+
+
+}
+menuBurger.init();
