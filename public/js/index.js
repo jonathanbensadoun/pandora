f2dea06b13bd859385b4e18ec864b0db9a542378
jonathanbensadoun <jonathan.ben-sadoun@oclock.school>
2023-11-25 18:57:34 +0100

    changement de la description de pandora , ajout des slide pour la page , au click sur la rocket  elle décolle avec toggle mais il faut 2 click pour refaire décollé du au toggle trouver une nouvelle solution

--- public/js/index.js
+++ public/js/index.js
@@ -6,11 +6,25 @@ const menuBurger = {
     init: function(){
         const btnMenuBurger = document.querySelector('.btn-menu');
         btnMenuBurger.addEventListener('click',menuBurger.handleClickMenuBurger);
-
-
-
     }
-
+}
+const takeOff ={
+    rocket : null,
+    handleClickRocket: function(event){
+        event.preventDefault();
+        rocket = document.querySelector('.a-logo');
+        console.log(rocket);
+        rocket.classList.add('takeoff')
+        rocket.classlits.remove('takeoff')
+    },
+    init: function(){
+        rocket = document.querySelector('.a-logo');
+        console.log(rocket);
+        rocket.addEventListener('click',this.handleClickRocket);
+    }
+}
+const slide ={
 
 }
 menuBurger.init();
+takeOff.init();
